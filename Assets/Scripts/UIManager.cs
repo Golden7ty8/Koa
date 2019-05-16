310bd125185e64cf8bbc4a6ceed06796f65306b7
GoldenWay <33757527+Golden7ty8@users.noreply.github.com>
2019-05-16 10:01:01 -0300

    Build 0.0.3.0 (Final)

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,6 +8,10 @@ public class UIManager : MonoBehaviour
 
     public GameObject mainMenuGroup;
     public GameObject devOptionsGroup;
+    public GameObject buildInfoText;
+    public GameObject controlsText;
+    public GameObject nextButton;
+    public GameObject startButton;
 
     // Start is called before the first frame update
     void Start()
@@ -32,5 +36,13 @@ public class UIManager : MonoBehaviour
         mainMenuGroup.SetActive(!setVisible);
         devOptionsGroup.SetActive(setVisible);
 
+    }
+
+    public void NextButtonPressed()
+    {
+        buildInfoText.SetActive(false);
+        nextButton.SetActive(false);
+        controlsText.SetActive(true);
+        startButton.SetActive(true);
     }
 }
